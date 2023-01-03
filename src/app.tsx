6b5a9b0b9df86542916a6f1968042d1c5459d6f8
CynthiaEstradaPuller <cynthia@puller.mx>
2023-01-03 14:10:17 -0600

    34-CardPedido-Feat: Se crea card para pedido y ruta

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,23 @@
 import { useState } from 'preact/hooks'
 import{ UserBar }from './components/user-bar'
 import './app.css'
+import { CardInformacion } from './components/cards/infornacion/component'
 
 export function App() {
 
+  const data={
+    idPedido: '14-1317321',
+    statusPedido: 'Entregado',
+    direccionPedido: 'Calle Tabachín 45, Talaquepaque, Jal. 857263', 
+    nombrePedidoCliente: 'Eugenia Castillo',
+    tipo: 'ruta',
+    progressRute: 20,
+  }
+
   return (
     <>
-  <UserBar />
+
+  <CardInformacion idPedido={data.idPedido} status={data.statusPedido} primerTexto={data.direccionPedido} segundoTexto={data.nombrePedidoCliente} progressRute={data.progressRute} tipo={data.tipo} />
     </>
   )
 }
