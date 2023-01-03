b12873849040d2df1b25d7aafaf2328cfedc802e
adan-itsz <adan1995a@gmail.com>
2023-01-03 14:29:56 -0600

    buttons and first components

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'preact/hooks'
 import{ UserBar }from './components/user-bar'
 import './app.css'
-
+import {Buttons} from './generalComponents/buttons'
 export function App() {
 
   return (
     <>
-  <UserBar />
+    <Buttons type='primary' text='jbj' textColor='red' accion={()=>console.log}/>
     </>
   )
 }
