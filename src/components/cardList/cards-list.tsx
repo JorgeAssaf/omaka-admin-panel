5c83e551e3abd0451852aef01ecbfe42fcdd1226
Jorge Assaf <jorgeassaf160@gmail.com>
2023-05-27 22:22:40 -0600

    feat: 🎸 add SeachInput

--- src/components/cardList/cards-list.tsx
+++ src/components/cardList/cards-list.tsx
@@ -1,66 +1,137 @@
-import { OrderType } from "../../types/typeOrders";
-import { cardPropsType } from "../../types/typesCards";
-import CardRutas from "../generalCards/rutas-card";
-import CardPedidos from "../generalCards/pedidos-card";
-import CardRepartidor from "../generalCards/repartidor-card";
-import './styles.css';
-import Loading from "../atoms/loading";
-import CardCliente from "../generalCards/cliente-card";
+import { OrderType } from '../../types/typeOrders'
+import { cardPropsType } from '../../types/typesCards'
+import CardRutas from '../generalCards/rutas-card'
+import CardPedidos from '../generalCards/pedidos-card'
+import CardRepartidor from '../generalCards/repartidor-card'
+import './styles.css'
+import Loading from '../atoms/loading'
+import CardCliente from '../generalCards/cliente-card'
+import { useState } from 'preact/hooks'
 
 type CardListInterface = {
-  data: any[];
-  tipo: string;
-  variant?: string;
-  cardProps?: cardPropsType;
-  loading?: boolean;
-  activeItem?: string;
-  onClickItem: (item: OrderType | any) => void;
-};
+  data: any[]
+  tipo: string
+  variant?: string
+  cardProps?: cardPropsType
+  loading?: boolean
+  activeItem?: string
+  onClickItem: (item: OrderType | any) => void
+}
 
-export const CardList = ({ data, tipo, cardProps, onClickItem, variant, loading, activeItem }: CardListInterface) => {
-  
-  if(loading)return(<Loading/>)
+export const CardList = ({
+  data,
+  tipo,
+  cardProps,
+  onClickItem,
+  variant,
+  loading,
+  activeItem,
+}: CardListInterface) => {
+  const [searchRuta, setSearchRuta] = useState('')
+  const [searchPedido, setSearchPedido] = useState('')
+  const [searchRepartidor, setSearchRepartidor] = useState('')
+  const [searchCliente, setSearchCliente] = useState('')
+
+  if (loading) return <Loading />
   switch (tipo) {
     case 'pedidos':
       return (
         <div className='listContainer'>
           {data.map((item) => {
-            return <CardPedidos activeItem={activeItem} variant={variant} cardProps={cardProps} onClick={(item)=>onClickItem && onClickItem(item)} data={item} />;
+            return (
+              <CardPedidos
+                activeItem={activeItem}
+                variant={variant}
+                cardProps={cardProps}
+                onClick={(item) => onClickItem && onClickItem(item)}
+                data={item}
+              />
+            )
           })}
         </div>
       )
-      break;
+      break
     case 'rutas':
+
       return (
         <div className='listContainer'>
-          {data.map((item) => {
-            return <CardRutas data={item} activeItem={activeItem} cardProps={cardProps} onClick={(item)=>onClickItem && onClickItem(item)} />;
-          })}
+          <div className='searchContainer'>
+            <input
+              className='searchInput'
+              type='text'
+              placeholder='Buscar ruta'
+              value={searchRuta}
+              onChange={(e) => setSearchRuta(e.currentTarget.value)}
+            />
+          </div>
+
+          {data
+            .filter((item) =>
+              item.nombreRuta.toLowerCase().includes(searchRuta.toLowerCase().trim()),
+            )
+            .map((item) => {
+              return (
+                <CardRutas
+                  data={item}
+                  activeItem={activeItem}
+                  cardProps={cardProps}
+                  onClick={(item) => onClickItem && onClickItem(item)}
+                />
+              )
+            })}
         </div>
       )
-      break;
+      break
     case 'repartidor':
+      console.log(data);
       return (
         <div className='listContainer onboarding-item-repartidor'>
-          {data.map((item) => {
-            return <CardRepartidor activeItem={activeItem} cardProps={cardProps} onClick={(item)=>onClickItem && onClickItem(item)} data={item} />;
-          })}
+          <div className='searchContainer'>
+            <input
+              className='searchInput'
+              type='text'
+              placeholder='Buscar repartidor'
+              value={searchRepartidor}
+              onChange={(e) => setSearchRepartidor(e.currentTarget.value)}
+            />
+          </div>
+          {data.filter((item) =>
+
+            item.DatosPersonales.nombre.toLowerCase() || item.DatosPersonales.apellido.toLowerCase()
+              .includes(searchRepartidor.toLowerCase().trim()),
+          )
+
+            .map((item) => {
+              return (
+                <CardRepartidor
+                  activeItem={activeItem}
+                  cardProps={cardProps}
+                  onClick={(item) => onClickItem && onClickItem(item)}
+                  data={item}
+                />
+              )
+            })}
         </div>
       )
-      break;
+      break
     case 'cliente':
       return (
         <div className='listContainer'>
           {data.map((item) => {
-            return <CardCliente activeItem={activeItem} cardProps={cardProps} onClick={(item)=>onClickItem && onClickItem(item)} data={item} />;
+            return (
+              <CardCliente
+                activeItem={activeItem}
+                cardProps={cardProps}
+                onClick={(item) => onClickItem && onClickItem(item)}
+                data={item}
+              />
+            )
           })}
         </div>
       )
-      break;
+      break
     default:
-      return(<></>);
-      break;
+      return <></>
+      break
   }
-  
-};
-
+}
